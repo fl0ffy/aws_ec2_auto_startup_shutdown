e612a8b9824228033ffc74c945c6ca8c86a97d73
fl0ffy <bernat.johnathan@gmail.com>
2021-09-17 09:30:39 -0400

    add IAM role resource

--- main.tf
+++ main.tf
@@ -1,12 +1,34 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 3.0"
-        }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+      }
     }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
+
+
+resource "aws_iam_role" "lambda_role" {
+  name = "iam_role_lambda_function"
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+          },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }
+  EOF
+}
+
+
